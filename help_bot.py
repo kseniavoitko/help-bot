e94e77db5381809f0ef117a99f49f7301789d5c9
kseniavoitko <40660518+kseniavoitko@users.noreply.github.com>
2023-07-09 10:59:17 +0300

    10 hw

--- help_bot.py
+++ help_bot.py
@@ -1,4 +1,65 @@
-contacts = {}
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    def add_record(self, rec):
+        has_record = self.get(rec.name.value, 0)
+        if has_record:
+            has_record.add_phones(rec.phones)
+            return "Phone saved"
+        else:
+            self[rec.name.value] = rec
+            return 'Contact saved'
+        
+    def show_all_records(self):
+        if not len(self):
+            return "No contacts"
+        return "\n".join([str(i) for i in self.values()])
+        
+    def search_record(self, key):
+        return self[key]
+    
+
+class Record():
+    def __init__(self, name, phones):
+        self.name = name
+        self.phones = phones
+
+    def __repr__(self):
+        return f'{self.name}: {", ".join([str(i) for i in self.phones])}'
+    
+    def change_phone(self, old, new):
+        old_ind = [i.value for i in self.phones].index(old)
+        self.phones[old_ind] = new
+        return 'Contact changed'
+    
+    def add_phones(self, phones):
+        for phone in phones:
+            self.phones.append(phone)
+
+    def remove_phone(self, phone):
+        phone_index = [i.value for i in self.phones].index(phone)
+        self.phones.pop(phone_index)
+        return 'Contact remove'
+
+
+class Field():
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f'{self.value}' 
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+addressbook = AddressBook()
 
 def input_error(func):
     def inner(args):
@@ -15,6 +76,9 @@ def input_error(func):
         except KeyError:
             if func.__name__ == 'phone' or func.__name__ == 'change':
                 return "Contact doesn't exist"
+        except ValueError:
+            if func.__name__ == 'change':
+                return "Contact doesn't exist"
     return inner
 
 
@@ -23,31 +87,34 @@ def hello(args):
 
 
 @input_error
-def add(args):
-    if contacts.get(args[0], 0):
-        return "Сontact already exists"
-    else:
-        contacts[args[0]] = args[1]
-        return 'Contact saved'
+def add(args):   
+    name = Name(args[0])
+    phones = []
+    for phone in args[1:]:
+        phones.append(Phone(phone))
+    rec = Record(name, phones)
+    return addressbook.add_record(rec)
 
 
 @input_error
 def change(args):
-    if contacts[args[0]]:
-        contacts[args[0]] = args[1]
-        return 'Contact changed'
+    """Get 2 phones to change
+    or 1 phone to remove"""
+    record = addressbook.search_record(args[0])
+    try:
+        new_phone = Phone(args[2])
+        return record.change_phone(args[1], new_phone)
+    except:
+        return record.remove_phone(args[1])
 
 
 @input_error
 def phone(args):
-    return contacts[args[0]]
+    return addressbook.search_record(args[0])
 
 
 def show_all(args):
-    if not len(contacts):
-        return "No contacts"
-    for key, value in contacts.items():
-        return key, value     
+    return addressbook.show_all_records()  
 
 
 def no_command(args):
